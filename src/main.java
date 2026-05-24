2d304b3f518d42641e0c5a24163ff8ab86ae88ad
SaturnusOps <harmandeeptung2@gmail.com>
2026-05-24 21:57:32 +0200

    executable

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         System.out.print("How many characters would you like?:");
